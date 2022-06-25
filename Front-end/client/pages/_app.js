70bb61a1d3891008d7ccda4707def6b066deee16
peemaphoj <peemaphoj@peemaphojs-MacBook-Air.local>
2022-06-25 16:45:58 +0700

    add login logic and get token

--- Front-end/client/pages/_app.js
+++ Front-end/client/pages/_app.js
@@ -1,9 +1,15 @@
-import '../styles/globals.css'
-import 'antd/dist/antd.css'
+import "../styles/globals.css";
+import "antd/dist/antd.css";
 import "../styles/app.css";
+import { ApolloProvider } from "@apollo/client/react";
+import client from "../src/config/initApollo";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- Front-end/client/pages/login.js
+++ Front-end/client/pages/login.js
@@ -1,17 +1,83 @@
 import Head from "next/head";
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Button, Checkbox, Form, Input } from "antd";
 import Navbar from "../components/navbar";
 import Footer from "../components/Footer";
+import { useMutation, useQuery, gql } from "@apollo/client";
+import Cookies from "js-cookie";
+
+const doLoginMutation = gql`
+  mutation ($input: LoginInput!) {
+    login(input: $input) {
+      success
+      token
+    }
+  }
+`;
+
+const url = 'http://localhost:3000/api/v1/auth/login'
 
 function login(props) {
-    const [email,setEmail] = useState('')
-    const [password, setPassword] = useState('')
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const onSubmit = () => {
-        console.log(email)
-        console.log(password)
+  const [login, { loading: mutationLoading, error: mutationError, data }] =
+    useMutation(doLoginMutation);
+
+  const loginWithGQL = async () => {
+    const loginInput = {
+      email: email,
+      password: password,
+    };
+
+    await login({ variables: { input: loginInput } });
+    console.log(mutationLoading);
+
+    if (data.login.success) {
+      console.log("data success");
+      document.cookie = "token=" + data.login.token;
     }
+
+    console.log(document.cookie);
+  };
+
+  const loginDirect = async () => {
+    const options = {
+      method: "post",
+      headers: {
+        "Content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+      },
+      body: `email=${email}&password=${password}`,
+    };
+
+    fetch(url, options)
+      .then((response) => {
+        if (!response.ok) {
+          if (response.status === 404) {
+            alert("Email not found, please retry");
+          }
+          if (response.status === 401) {
+            alert("Email and password do not match, please retry");
+          }
+        }
+        return response;
+      })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.success) {
+          document.cookie = "token=" + data.token;
+          console.log(document.cookie);
+        }
+      });
+  };
+
+  const onSubmit = async () => {
+    console.log(email);
+    console.log(password);
+
+    // loginWithGQL();
+    loginDirect();
+  };
 
   return (
     <>
@@ -39,7 +105,7 @@ function login(props) {
           rules={[{ required: true, message: "Please input your email!" }]}
           style={{ width: "40%" }}
         >
-          <Input type='email' onChange={e => setEmail(e.target.value)} />
+          <Input type="email" onChange={(e) => setEmail(e.target.value)} />
         </Form.Item>
 
         <Form.Item
@@ -48,7 +114,7 @@ function login(props) {
           rules={[{ required: true, message: "Please input your password!" }]}
           style={{ width: "40%" }}
         >
-          <Input.Password onChange={e => setPassword(e.target.value)}/>
+          <Input.Password onChange={(e) => setPassword(e.target.value)} />
         </Form.Item>
         {/* 
       <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
@@ -61,7 +127,7 @@ function login(props) {
           </Button>
         </Form.Item>
       </Form>
-
+      {/* <p>{Cookies.get('token')}</p> */}
       <Footer />
     </>
   );
